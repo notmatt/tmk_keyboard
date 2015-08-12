94a068b4c2eb8115dcd4e21911f0d588cb97d611
Matt Smillie <matt.smillie@joyent.com>
2015-08-12 00:33:32 -0700

    reorg, sticky mods.

--- keyboard/ergodox/keymap_colemak.h
+++ keyboard/ergodox/keymap_colemak.h
@@ -3,23 +3,23 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * Keymap: default lay in COLEMAK-ish, nut missing quotes, braces (see l2)
      *
      * +-----+---+---+---+---+---+---+    +---+---+---+---+---+---+-----+
-     * | esc | 1 | 2 | 3 | 4 | 5 | ` |    | = | 6 | 7 | 8 | 9 | 0 | bak |
+     * | esc | 1 | 2 | 3 | 4 | 5 | - |    | = | 6 | 7 | 8 | 9 | 0 | udo |
      * +-----+---+---+---+---+---+---+    +---|---+---+---+---+---+-----+
-     * | tab | Q | W | F | P | G |   |    |   | J | L | U | Y | ; |  |  |
-     * +-----+---+---+---+---+---|bak|    |CMD|---+---+---+---+---+-----+
+     * | tab | Q | W | F | P | G |   |    |   | J | L | U | Y | ; | bak |
+     * +-----+---+---+---+---+---|bak|    |CTL|---+---+---+---+---+-----+
      * | ctl | A | R | S | T | D |---+    +---| H | N | E | I | O | ent |
      * +-----+---+---+---+---+---+   |    |   +---+---+---+---+---+-----+
-     * | sft | Z | X | C | V | B |sL2|    |sL2| K | M | < | > | ? | sft |
+     * | sft | Z | X | C | V | B | NO|    |CMD| K | M | < | > | ? | sft |
      * +-----+---+---+---+---+---+---+    +---+---+---+---+---+---+-----+
-     *       |   |   |   |   |sL2|            |sL2|lft|dwn| up|rgt|
+     *       |fun|alt|cmd|ctl|sL2|            |sL2|lft|dwn| up|rgt|
      *       +---+---+---+---+---+            +---+---+---+---+---+
      *
      *                     +---+---+    +---+---+
-     *                     |+L1|sL3|    |   |   |
+     *                     |+L1|no |    |no |no |
      *                 +---+---+---+    +---+---+---+
-     *                 |   |   |alt|    |alt|   |   |
+     *                 |   |   |no |    |no |   |   |
      *                 |cmd|sft|---+    +---|ent|spc|
-     *                 |   |   |ctl|    |ctl|(1)|   |
+     *                 |   |   |no |    |no |(1)|   |
      *                 +---+---+---+    +---+---+---+
      *
      * (1) 'sL2' - layer modification.
@@ -50,15 +50,15 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * Keymap: Layer 2 is a symbol/punctuation layer
      *
      * +-----+---+---+---+---+---+---+    +---+---+---+---+---+---+-----+
-     * |     |   |   |   |   |   |   |    |   |   |   |   |   |   |     |
+     * |     |F1 |F2 |F3 |F4 |F5 |F6 |    |F7 |F8 |F9 |F10|F11|F12|     |
      * +-----+---+---+---+---+---+---+    +---|---+---+---+---+---+-----+
-     * |     |   | | | & | * |   |   |    |   |   | { | } |   |   |     |
+     * |     |   | | | & | * | ` |   |    |   |   | { | } |   |   |     |
      * +-----+---+---+---+---+---|   |    |   |---+---+---+---+---+-----+
      * |     |   | + | - | = | @ |---+    +---| ' | ( | ) | " |   |     |
      * +-----+---+---+---+---+---+   |    |   +---+---+---+---+---+-----+
      * |     |   | % | / | ! | # |   |    |   | \ | [ | ] | / |   |     |
      * +-----+---+---+---+---+---+---+    +---+---+---+---+---+---+-----+
-     *       |   |   |   |   |   |            |   |   |   |   |   |
+     *       |   |   |   |   |   |            |   |prv|   |   |nxt|
      *       +---+---+---+---+---+            +---+---+---+---+---+
      *
      *                     +---+---+    +---+---+
@@ -97,23 +97,23 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     KEYMAP( // L0: default
-         ESC,    1,    2,    3,    4,    5,  GRV,
+         ESC,    1,    2,    3,    4,    5, MINS,
          TAB,    Q,    W,    F,    P,    G, BSPC,
-        LCTL,    A,    R,    S,    T,    D,
-        LSFT,    Z,    X,    C,    V,    B, FN17,
-          NO,   NO,   NO,   NO, FN17,
-                                       FN1, FN18,
-                                            LALT,
-                                LGUI, FN17, LCTL,
+        FN23,    A,    R,    S,    T,    D,
+        FN24,    Z,    X,    C,    V,    B, FN25,
+         FN4, FN21, FN22, FN23,  FN2,
+                                       FN1,  FN4,
+                                              NO,
+                                LGUI,  FN2,   NO,
 
-         EQL,    6,    7,    8,    9,    0, BSPC,
-        RGUI,    J,    L,    U,    Y, SCLN, BSLS,
+         EQL,    6,    7,    8,    9,    0,   NO,
+        FN23,    J,    L,    U,    Y, SCLN,  BAK,
                  H,    N,    E,    I,    O,  ENT,
-        FN17,    K,    M, COMM,  DOT, SLSH, RSFT,
-                    FN17, LEFT, DOWN,   UP, RGHT,
+        FN22,    K,    M, COMM,  DOT, SLSH, FN24,
+                     FN2, LEFT, DOWN,   UP, RGHT,
           NO,   NO,
         RALT,
-        RCTL, FN17,  SPC
+        RCTL,  FN2,  FN3
     ),
 
     KEYMAP( // L1: QWERTY
@@ -137,23 +137,23 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     KEYMAP( // L2: punct
-        TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
-        TRNS, TRNS,  FN4,  FN3,  FN5, TRNS, TRNS,
-        TRNS, TRNS,  FN7, MINS,  EQL, FN19,
-        TRNS, TRNS,  FN9, SLSH, FN10, FN20, TRNS,
+        TRNS,   F1,   F2,   F3,   F4,   F5,   F6,
+        TRNS, TRNS,  FN7,  FN6,  FN8,  GRV, TRNS,
+        TRNS, TRNS, FN10, MINS,  EQL, FN20,
+        TRNS, TRNS, FN12, SLSH, FN13,  FN5, TRNS,
         TRNS, TRNS, TRNS, TRNS, TRNS,
                                       TRNS, TRNS,
                                             TRNS,
                                 TRNS, TRNS, TRNS,
 
-        TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
-        TRNS, TRNS, FN11, FN12, TRNS, TRNS, TRNS,
-              QUOT, FN13, FN14, FN15, SCLN, TRNS,
+          F7,   F8,   F9,  F10,  F11,  F12, TRNS,
+        TRNS,  GRV, FN14, FN15, TRNS, TRNS, TRNS,
+              QUOT, FN16, FN17, FN18, SCLN, TRNS,
         TRNS, BSLS, LBRC, RBRC, SLSH, TRNS, TRNS,
-                    TRNS, TRNS, TRNS, TRNS, TRNS,
+                    TRNS, FN27, TRNS, TRNS, FN28,
         TRNS, TRNS,
         TRNS,
-        TRNS, TRNS, FN16
+        TRNS, TRNS, TRNS
     ),
 
     KEYMAP( // L3: media
@@ -170,7 +170,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS, TRNS, MUTE, VOLD, VOLU, TRNS, TRNS,
               TRNS, MPRV, MPLY, MNXT, TRNS, TRNS,
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,
-                    TRNS, TRNS, TRNS, TRNS, TRNS,
+                    TRNS, FN27, TRNS, TRNS, FN28,
         TRNS, TRNS,
         TRNS,
         TRNS, TRNS, TRNS
@@ -178,31 +178,39 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 static const uint16_t PROGMEM fn_actions[] = {
-    ACTION_DEFAULT_LAYER_SET(0),    // Set Layer 0 (Colematt)
-    ACTION_DEFAULT_LAYER_SET(1),     // Set Layer 1 (Qwerty)
+    ACTION_DEFAULT_LAYER_SET(0),             // Set Layer 0 (Colematt)
+    ACTION_DEFAULT_LAYER_SET(1),             // Set Layer 1 (Qwerty)
+    ACTION_LAYER_MOMENTARY(2),               // FN2 - punct
+    ACTION_LAYER_TAP_KEY(2, KC_SPC)          // FN3 - tap-space, hold punct
+    ACTION_LAYER_MOMENTARY(3),               // FN4 - media
 
     /* punctuation layer */
-    ACTION_MODS_KEY(MOD_LSFT, KC_3),         // # FN2
-    ACTION_MODS_KEY(MOD_LSFT, KC_7),         // & FN3
-    ACTION_MODS_KEY(MOD_LSFT, KC_BSLASH),    // | FN4
-    ACTION_MODS_KEY(MOD_LSFT, KC_8),         // * FN5
-    ACTION_MODS_KEY(MOD_LSFT, KC_6),         // ^ FN6
-    ACTION_MODS_KEY(MOD_LSFT, KC_EQUAL),     // + FN7
-    ACTION_MODS_KEY(MOD_LSFT, KC_GRV),       // ~ FN8
-    ACTION_MODS_KEY(MOD_LSFT, KC_5),         // % FN9
-    ACTION_MODS_KEY(MOD_LSFT, KC_1),         // ! FN10
-    ACTION_MODS_KEY(MOD_LSFT, KC_LBRACKET),  // { FN11
-    ACTION_MODS_KEY(MOD_LSFT, KC_RBRACKET),  // } FN12
-    ACTION_MODS_KEY(MOD_LSFT, KC_9),         // ( FN13
-    ACTION_MODS_KEY(MOD_LSFT, KC_0),         // ) FN14
-    ACTION_MODS_KEY(MOD_LSFT, KC_QUOT),      // " FN15
-    ACTION_MODS_KEY(MOD_LSFT, KC_MINUS),     // _ FN16
+    ACTION_MODS_KEY(MOD_LSFT, KC_3),         // # FN5
+    ACTION_MODS_KEY(MOD_LSFT, KC_7),         // & FN6
+    ACTION_MODS_KEY(MOD_LSFT, KC_BSLASH),    // | FN7
+    ACTION_MODS_KEY(MOD_LSFT, KC_8),         // * FN8
+    ACTION_MODS_KEY(MOD_LSFT, KC_6),         // ^ FN9
+    ACTION_MODS_KEY(MOD_LSFT, KC_EQUAL),     // + FN10
+    ACTION_MODS_KEY(MOD_LSFT, KC_GRV),       // ~ FN11
+    ACTION_MODS_KEY(MOD_LSFT, KC_5),         // % FN12
+    ACTION_MODS_KEY(MOD_LSFT, KC_1),         // ! FN13
+    ACTION_MODS_KEY(MOD_LSFT, KC_LBRACKET),  // { FN14
+    ACTION_MODS_KEY(MOD_LSFT, KC_RBRACKET),  // } FN15
+    ACTION_MODS_KEY(MOD_LSFT, KC_9),         // ( FN16
+    ACTION_MODS_KEY(MOD_LSFT, KC_0),         // ) FN17
+    ACTION_MODS_KEY(MOD_LSFT, KC_QUOT),      // " FN18
+    ACTION_MODS_KEY(MOD_LSFT, KC_MINUS),     // _ FN19
+    ACTION_MODS_KEY(MOD_LSFT, KC_2),         // @ FN20
 
-    ACTION_LAYER_MOMENTARY(2),               // FN17 - punct
-    ACTION_LAYER_MOMENTARY(3),               // FN18 - media
+    ACTION_MODS_ONESHOT(MOD_LALT),           // FN21 - oneshot alt
+    ACTION_MODS_ONESHOT(MOD_LGUI),           // FN22 - oneshot cmd
+    ACTION_MODS_ONESHOT(MOD_LCTL),           // FN23 - oneshot ctrl
+    ACTION_MODS_ONESHOT(MOD_LSFT),           // FN24 - oneshot shift
+    ACTION_MODS_TAP_TOGGLE(MOD_LSFT),        // FN25 - tap-toggle shift
+    ACTION_MODS_KEY(MOD_LALT | MOD_LSFT),    // FN26 - alt-shift 'select'
 
-    ACTION_MODS_KEY(MOD_LSFT, KC_2),         // FN19 - @
-    ACTION_MODS_KEY(MOD_LSFT, KC_3)          // FN20 - #
+    ACTION_MODS_KEY(MOD_LGUI | MOD_LSFT, KC_LBRACKT),   // FN27 - cmd-{
+    ACTION_MODS_KEY(MOD_LGUI | MOD_LSFT, KC_RBRACKET)   // FN28 - cnd-}
 };
 
 // void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
